991f19de814aafe6bfecfc629bf548f5f888f27d
Arjuna <arjuna.uop@gmail.com>
2018-07-17 22:27:39 +0530

    boostrap added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { theme } from 'admin-lte';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+// @theme({
+//   selector:'app-theme',
+//   templateUrl:'./app.index.html'
+// })
+
 export class AppComponent {
-  title = 'app';
+  title = 'Odometer';
+  name = 'jkjkl';
 }
